fb403c29fbecd6e9e880d78ae6fe03e4c445a8e0
son2313 <hoangsonngo555@gmail.com>
2021-06-23 23:46:40 +0700

    Added mail search featrure

--- MailClient/UserControls/MailNavigationControl.xaml.cs
+++ MailClient/UserControls/MailNavigationControl.xaml.cs
@@ -1,11 +1,20 @@
-﻿using MailClient.Helpers;
+﻿using MailClient.DataModels.Mail;
+using MailClient.Helpers;
 using System;
+using System.Linq;
+using System.Collections.ObjectModel;
 using Windows.UI.Xaml.Controls;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace MailClient.UserControls
 {
     public sealed partial class MailNavigationControl : UserControl
     {
+        private ObservableCollection<MailMessage> MailMessageItems;
+
+        private ObservableCollection<MailMessage> FilteredMailMessageItems = new();
+
         public delegate void EvenHandler(object sender, EventArgs e);
 
         public event EventHandler OnMailMessageSelected;
@@ -13,6 +22,15 @@ namespace MailClient.UserControls
         public MailNavigationControl()
         {
             this.InitializeComponent();
+
+            Loaded += MailNavigationControl_Loaded;
+        }
+
+        private void MailNavigationControl_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            MailMessageItems = new(AccountHelper.CurretMailBoxMessages);
+
+            SearchBox_TextChanged(null, null);
         }
 
         public bool IsLoadingBarRun
@@ -28,6 +46,34 @@ namespace MailClient.UserControls
             {
                 OnMailMessageSelected(AccountHelper.CurretMailBoxMessages[listView.SelectedIndex], null);
             }
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (MainListView.ItemsSource != FilteredMailMessageItems)
+                MainListView.ItemsSource = FilteredMailMessageItems;
+
+            if (MainListView.ItemsSource == FilteredMailMessageItems)
+            {
+                var filtered = MailMessageItems.Where(x => Filter(x, SearchBox.Text)).ToArray();
+                foreach (MailMessage i in FilteredMailMessageItems.ToArray())
+                {
+                    if (!filtered.Any(x => i.From == x.From && i.Subject == x.Subject))
+                        FilteredMailMessageItems.Remove(i);
+                }
+
+                for (int i = 0; i < filtered.Length; i++)
+                {
+                    if (!FilteredMailMessageItems.Any(x => filtered[i].From == x.From && filtered[i].Subject == x.Subject))
+                        FilteredMailMessageItems.Insert(i > FilteredMailMessageItems.Count ? FilteredMailMessageItems.Count : i, filtered[i]);
+                }
+            }
+        }
+
+        private bool Filter(MailMessage mailMessage, string value)
+        {
+            if (value == string.Empty) return true;
+            return mailMessage.Subject.Contains(value) || mailMessage.From.Contains(value) || mailMessage.GetToString().Contains(value);
         }
     }
 }
